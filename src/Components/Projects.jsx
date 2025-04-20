7cf0fd6d76a75ffb7440cca11b4abbe7b291b7bd
aman812642 <amanpleo98378@gmail.com>
2025-04-20 10:50:07 +0530

    Updated index.html and Projects.jsx, added Taskflow image

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,6 +5,7 @@ import premier from '../assets/Premier.png'
 import refokus from '../assets/Refokus.png'
 import ecommerce from '../assets/Ecommerce.png'
 import rental from '../assets/Rentals.png'
+import taskflow from '../assets/Taskflow.png'
 
 
 function Projects() {
@@ -15,15 +16,18 @@ function Projects() {
         {images : ecommerce  , link : "https://ekart-ecommerce-web.netlify.app/", projectName : "Ecommerce webpage" , responsive : true , projectDescription : "Crafted a fully responsive E-commerce Webpage using HTML, CSS, and JavaScript. This project features an intuitive user interface, seamless navigation, and dynamic content rendering to enhance user experience across all devices." , techUsed : [
             "html" , "css" , "javascript"
         ]},
-        {images : refokus, projectName : "Refokus" , responsive : false , projectDescription : "I created a clone of Refokus website work page in which is use React Js , Tailwind css ,  Locomotive and Framer motion " , techUsed : [
-            "React Js" , "Tailwind Css" , "Framer Motion" , "Locomotive"
-        ]},
         {images : premier, link : "https://primer-responsive.netlify.app/",projectName : "Premier Webpage"  , responsive : true ,  projectDescription : "This is a landing page of premier website i created it using html css only and also add autoprifix css code to it which helps to support this webpage in all browsers .." , techUsed : [
             "html" , "css" , "javascript"
         ]},
         {images : rental, link : "https://rentals-your.netlify.app", projectName : "Rent Your Property"  , responsive : true ,  projectDescription : "I created this website this is a clone of Airbnb I which we can see propterties which are avilable for rent and also add our property dynamically" , techUsed : [
             "React js" , "css" , "Emailjs"
-        ]}
+        ]},
+        {images : taskflow, link : "https://employeemanagementc.netlify.app/", projectName : "Task Flow"  , responsive : true ,  projectDescription : "I'm excited to share my latest project, TaskFlow, a task management application designed for both admins and employees to efficiently manage tasks, track progress, and boost productivity And Uses Localstorage for storing data in browser also also get data from browser" , techUsed : [
+          "React js" , "Tailwind css"
+      ]},
+      {images : refokus, projectName : "Refokus" , responsive : false , projectDescription : "I created a clone of Refokus website work page in which is use React Js , Tailwind css ,  Locomotive and Framer motion " , techUsed : [
+        "React Js" , "Tailwind Css" , "Framer Motion" , "Locomotive"
+    ]},
     ]
   return (
     <div>
